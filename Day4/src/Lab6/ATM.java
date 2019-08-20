7a53f6326b1bd3cb22a62bfdc1d709c6f9265cf7
hothuythuthao <54274040+hothuythuthao@users.noreply.github.com>
2019-08-20 20:14:01 +0700

    add new day 4

--- Day4/src/Lab6/ATM.java
+++
@@ -1,9 +0,0 @@
-package Lab6;
-
-public interface ATM {
-	boolean withdraw(int accID, double money);
-	boolean deposit(int accID, double money);
-	double queryBalance(int accID);
-	boolean login(String account, String password);
-	boolean logout(String account);
-}
